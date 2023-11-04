9efa9da5cdb1ff165685f772c36be40f99e5105c
jose99991982 <40325762+jose99991982@users.noreply.github.com>
2023-11-04 15:45:20 -0500

    Update fra.js

--- fra.js
+++ fra.js
@@ -1,1 +1,1 @@
-document.write('<div class="respon"><iframe src="' + uc + '" width="100%" height="100%" scrolling="no" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>');
+document.write('<div class="respon"><iframe data-src="' + uc + '" width="100%" height="100%" scrolling="no" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>');
