a57324fc28ca36e3bbdf6dba6ed55f75cfc92eb7
jose99991982 <40325762+jose99991982@users.noreply.github.com>
2023-11-04 15:44:50 -0500

    Create fra.js

---
+++ fra.js
@@ -0,0 +1,1 @@
+document.write('<div class="respon"><iframe src="' + uc + '" width="100%" height="100%" scrolling="no" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>');
